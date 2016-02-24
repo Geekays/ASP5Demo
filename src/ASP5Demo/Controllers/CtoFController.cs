35ccfda4b65236dd90066fed039f407ee889a6f4
Geekays <geekays@gmail.com>
2016-02-24 16:23:25 +0530

    Added CtoFController

---
+++ src/ASP5Demo/Controllers/CtoFController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP5Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class CtoFController : Controller
+    {
+        // GET: api/CtoF
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "This is a converter from C to F" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
