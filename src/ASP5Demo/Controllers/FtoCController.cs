d2406b2c1f20937a87b430b2e9e49448116aad4c
Geekays <geekays@gmail.com>
2016-02-25 12:17:37 +0530

    Added FtoC Controller

---
+++ src/ASP5Demo/Controllers/FtoCController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP5Demo.Controllers
+{
+    [Route("api/[controller]")]
+    public class FtoCController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "This is a converter for F to C" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{f}")]
+        public string Get(int f)
+        {
+            return ((f-32)*5/9).ToString();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
