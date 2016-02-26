455a751bf412381e24407561abc99a95edf3acc2
Geekays <geekays@gmail.com>
2016-02-26 12:10:36 +0530

    Added Unit Test project and added one unit test for CtoF

--- ASP5DemoUT/Controllers/CtoFTest.cs
+++ ASP5DemoUT/Controllers/CtoFTest.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
+using ASP5Demo.Controllers;
+
 
 namespace ASP5DemoUT
 {
@@ -9,8 +12,19 @@ namespace ASP5DemoUT
     // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
     public class CtoFTest
     {
+        private readonly ASP5Demo.Controllers.CtoFController _ctrl;
+
         public CtoFTest()
         {
+            _ctrl = new CtoFController();
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void CtoFTestCheck(int value)
+        {
+            string f= _ctrl.Get(value);
+            Assert.True(f == "212");
         }
     }
 }
